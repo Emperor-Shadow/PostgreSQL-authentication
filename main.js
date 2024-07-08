3dfaae66f99ee0982162095e3d6c82ac326df102
Codebot <damilolasaheeb@gmail.com>
2024-07-08 19:02:26 +0100

    111

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const prisma = new PrismaClient();
 const SECRET =  process.env.JWT_SECRET;
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
 app.use(express.json());
