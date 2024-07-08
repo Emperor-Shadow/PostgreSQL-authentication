04ba09c27316b709059d26a9df84d4568da7eb7e
Codebot <damilolasaheeb@gmail.com>
2024-07-08 19:17:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -266,3 +266,5 @@ app.post('/api/organisations/:orgId/users', verifyToken, async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
